b49a6753ca05317305da8fa62d3fae3042c9ec05
Anastasia <anaaustin1775@gmail.com>
2018-06-23 22:01:41 +0300

    Find the number of days in a winter

---
+++ WinterAndSummer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterAndSummer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter temperature values separated by spaces");
+                var input = Console.ReadLine().Split(' ');
+                var temperaturesArray = new int[input.Length];
+                for(int i = 0; i < input.Length; i++)
+                {
+                    if (!int.TryParse(input[i], out int temperature))
+                    {
+                        Console.WriteLine("Please check the input and try again.");
+                        Console.ReadKey();
+                        return;
+                    }
+                    temperaturesArray[i] = temperature;
+                }
+                var result = solution.WinterAndSummer(temperaturesArray);
+                Console.WriteLine(result);
+                Console.Write("Repeat? (y/n)");
+
+            } while (ShouldRepeat());
+        }
+
+        static bool ShouldRepeat()
+        {
+            return Console.ReadKey(false).Key == ConsoleKey.Y;
+        }
+    }
+}
---
+++ WinterAndSummer/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterAndSummer
+{
+    public class Solution
+    {
+        public int WinterAndSummer(int[] t)
+        {
+            var maxTempInWinter = t[0];
+            int winterLength = 0;
+            foreach(int temp in t)
+            {
+                if(temp <= maxTempInWinter)
+                {
+                    maxTempInWinter = Math.Max(temp, maxTempInWinter);
+                    winterLength++;
+                }
+            }
+            return winterLength;
+        }
+    }
+}
